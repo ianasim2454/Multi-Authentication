8bff4b80a45a3f60b15f9d151b9228c02349d872
ianasim2454 <ianasim191@gmail.com>
2024-05-20 09:29:03 +0600

    Laravel Multi Authenticatin App

---
+++ multiauth/app/Http/Controllers/AdminController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    public function AdminDashboard(){
+        return view('backend.dashboard');
+    }
+
+    public function AdminLogOut(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
